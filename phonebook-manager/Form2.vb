e564553d800befa68196cb75e5247f6f302c1309
Kevin A Mathew <kevinam99@gmail.com>
2019-03-02 17:19:02 +0530

    Created a new screen for selecting options. Added four button: create, search, edit and delete

--- phonebook-manager/Form2.vb
+++ phonebook-manager/Form2.vb
@@ -1,3 +1,12 @@
-﻿Public Class Form2
+﻿Public Class OptionsScreen
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
 
 End Class
