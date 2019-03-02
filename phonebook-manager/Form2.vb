9417c4b0a063cdb6f25499c7b52e1c7f5f3c2476
Kevin A Mathew <kevinam99@gmail.com>
2019-03-02 19:03:32 +0530

    Added actions for 'create new contact' button

--- phonebook-manager/Form2.vb
+++ phonebook-manager/Form2.vb
@@ -15,4 +15,10 @@
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         Process.Start("https://github.com/kevinam99/phonebook-manager")
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Create_Contact.Show()
+
+    End Sub
 End Class
