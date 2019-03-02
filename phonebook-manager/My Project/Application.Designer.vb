d1cad34ff5af90e11ff6f078dd1d185be657719f
Kevin A Mathew <kevinam99@gmail.com>
2019-03-02 17:26:12 +0530

    Created splash screen and select options screen. Looping has been taken care of and the app closes on clicking the close button

--- phonebook-manager/My Project/Application.Designer.vb
+++ phonebook-manager/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.phonebook_manager.Phonebook
+            Me.MainForm = Global.phonebook_manager.SplashScreen
         End Sub
     End Class
 End Namespace
