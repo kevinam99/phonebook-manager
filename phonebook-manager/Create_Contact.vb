9f82a3f527fc9e55b296b1ea4dc6a6431880731b
Kevin A Mathew <kevinam99@gmail.com>
2019-03-02 20:35:12 +0530

    Added 'Name' field

---
+++ phonebook-manager/Create_Contact.vb
@@ -0,0 +1,13 @@
+﻿Public Class Create_Contact
+    Private Sub SourceCode_LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles SourceCode_LinkLabel1.LinkClicked
+        Process.Start("https://github.com/kevinam99/phonebook-manager")
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles CreatName_Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
