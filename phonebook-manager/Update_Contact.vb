7d64611470e21708faff334eeb797c4dec309ec3
Kevin A Mathew <kevinam99@gmail.com>
2019-03-09 19:30:39 +0530

    Added link to source code.

--- phonebook-manager/Update_Contact.vb
+++ phonebook-manager/Update_Contact.vb
@@ -2,4 +2,8 @@
     Private Sub SearchHeading_Label1_Click(sender As Object, e As EventArgs) Handles SearchHeading_Label1.Click
 
     End Sub
+
+    Private Sub SourceCode_LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles SourceCode_LinkLabel1.LinkClicked
+        Process.Start("https://github.com/kevinam99/phonebook-manager")
+    End Sub
 End Class
