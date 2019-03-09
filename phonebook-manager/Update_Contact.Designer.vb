dd2f6a67001f4a903c29dcacf0432f41ba4969b0
Kevin A Mathew <kevinam99@gmail.com>
2019-03-09 19:33:56 +0530

    Resized window and icons.

--- phonebook-manager/Update_Contact.Designer.vb
+++ phonebook-manager/Update_Contact.Designer.vb
@@ -38,7 +38,7 @@ Partial Class Update_Contact
         '
         Me.SearchHeading_Label1.AutoSize = True
         Me.SearchHeading_Label1.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.SearchHeading_Label1.Location = New System.Drawing.Point(178, 9)
+        Me.SearchHeading_Label1.Location = New System.Drawing.Point(183, 9)
         Me.SearchHeading_Label1.Name = "SearchHeading_Label1"
         Me.SearchHeading_Label1.Size = New System.Drawing.Size(211, 36)
         Me.SearchHeading_Label1.TabIndex = 2
@@ -48,7 +48,7 @@ Partial Class Update_Contact
         'UpdateName_TextBox1
         '
         Me.UpdateName_TextBox1.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.UpdateName_TextBox1.Location = New System.Drawing.Point(221, 75)
+        Me.UpdateName_TextBox1.Location = New System.Drawing.Point(239, 73)
         Me.UpdateName_TextBox1.Name = "UpdateName_TextBox1"
         Me.UpdateName_TextBox1.Size = New System.Drawing.Size(283, 26)
         Me.UpdateName_TextBox1.TabIndex = 5
@@ -58,7 +58,7 @@ Partial Class Update_Contact
         Me.UpdateName_Label1.AutoSize = True
         Me.UpdateName_Label1.BackColor = System.Drawing.Color.Transparent
         Me.UpdateName_Label1.Font = New System.Drawing.Font("Constantia", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.UpdateName_Label1.Location = New System.Drawing.Point(29, 75)
+        Me.UpdateName_Label1.Location = New System.Drawing.Point(47, 73)
         Me.UpdateName_Label1.Name = "UpdateName_Label1"
         Me.UpdateName_Label1.Size = New System.Drawing.Size(66, 26)
         Me.UpdateName_Label1.TabIndex = 4
@@ -67,7 +67,7 @@ Partial Class Update_Contact
         'UpdateEmail_TextBox3
         '
         Me.UpdateEmail_TextBox3.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.UpdateEmail_TextBox3.Location = New System.Drawing.Point(221, 194)
+        Me.UpdateEmail_TextBox3.Location = New System.Drawing.Point(239, 192)
         Me.UpdateEmail_TextBox3.Name = "UpdateEmail_TextBox3"
         Me.UpdateEmail_TextBox3.Size = New System.Drawing.Size(283, 26)
         Me.UpdateEmail_TextBox3.TabIndex = 11
@@ -75,7 +75,7 @@ Partial Class Update_Contact
         'UpdateContactNumber_TextBox2
         '
         Me.UpdateContactNumber_TextBox2.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.UpdateContactNumber_TextBox2.Location = New System.Drawing.Point(221, 137)
+        Me.UpdateContactNumber_TextBox2.Location = New System.Drawing.Point(239, 135)
         Me.UpdateContactNumber_TextBox2.Name = "UpdateContactNumber_TextBox2"
         Me.UpdateContactNumber_TextBox2.Size = New System.Drawing.Size(283, 26)
         Me.UpdateContactNumber_TextBox2.TabIndex = 10
@@ -85,7 +85,7 @@ Partial Class Update_Contact
         Me.UpdateNumber_Label2.AutoSize = True
         Me.UpdateNumber_Label2.BackColor = System.Drawing.Color.Transparent
         Me.UpdateNumber_Label2.Font = New System.Drawing.Font("Constantia", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.UpdateNumber_Label2.Location = New System.Drawing.Point(29, 136)
+        Me.UpdateNumber_Label2.Location = New System.Drawing.Point(47, 134)
         Me.UpdateNumber_Label2.Name = "UpdateNumber_Label2"
         Me.UpdateNumber_Label2.Size = New System.Drawing.Size(159, 26)
         Me.UpdateNumber_Label2.TabIndex = 9
@@ -96,7 +96,7 @@ Partial Class Update_Contact
         Me.UpdateEmail_Label1.AutoSize = True
         Me.UpdateEmail_Label1.BackColor = System.Drawing.Color.Transparent
         Me.UpdateEmail_Label1.Font = New System.Drawing.Font("Constantia", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.UpdateEmail_Label1.Location = New System.Drawing.Point(29, 193)
+        Me.UpdateEmail_Label1.Location = New System.Drawing.Point(47, 191)
         Me.UpdateEmail_Label1.Name = "UpdateEmail_Label1"
         Me.UpdateEmail_Label1.Size = New System.Drawing.Size(64, 26)
         Me.UpdateEmail_Label1.TabIndex = 8
@@ -105,7 +105,7 @@ Partial Class Update_Contact
         'Exit_Button2
         '
         Me.Exit_Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Exit_Button2.Location = New System.Drawing.Point(311, 263)
+        Me.Exit_Button2.Location = New System.Drawing.Point(329, 248)
         Me.Exit_Button2.Name = "Exit_Button2"
         Me.Exit_Button2.Size = New System.Drawing.Size(193, 33)
         Me.Exit_Button2.TabIndex = 14
@@ -115,7 +115,7 @@ Partial Class Update_Contact
         'Save_Button1
         '
         Me.Save_Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Save_Button1.Location = New System.Drawing.Point(34, 263)
+        Me.Save_Button1.Location = New System.Drawing.Point(52, 248)
         Me.Save_Button1.Name = "Save_Button1"
         Me.Save_Button1.Size = New System.Drawing.Size(193, 33)
         Me.Save_Button1.TabIndex = 13
@@ -127,7 +127,7 @@ Partial Class Update_Contact
         Me.SourceCode_LinkLabel1.AutoSize = True
         Me.SourceCode_LinkLabel1.BackColor = System.Drawing.Color.Transparent
         Me.SourceCode_LinkLabel1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.SourceCode_LinkLabel1.Location = New System.Drawing.Point(492, 332)
+        Me.SourceCode_LinkLabel1.Location = New System.Drawing.Point(512, 290)
         Me.SourceCode_LinkLabel1.Name = "SourceCode_LinkLabel1"
         Me.SourceCode_LinkLabel1.Size = New System.Drawing.Size(73, 13)
         Me.SourceCode_LinkLabel1.TabIndex = 12
@@ -138,7 +138,7 @@ Partial Class Update_Contact
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(586, 366)
+        Me.ClientSize = New System.Drawing.Size(597, 313)
         Me.Controls.Add(Me.Exit_Button2)
         Me.Controls.Add(Me.Save_Button1)
         Me.Controls.Add(Me.SourceCode_LinkLabel1)
